25fb5a4f404cc60cb18d3e57f8a46a8636f2db14
kakurkova <kakurkova@edu.hse.ru>
2017-06-17 09:33:37 +0300

    THE LAST CHANGES

--- server.R
+++ server.R
@@ -1,3 +1,4 @@
+library(shiny)
 library(ggfortify)
 library(plotly)
 library(ggplot2)
@@ -11,23 +12,23 @@ library("ggmap")
 library("tidyr")
 library("dplyr")
 
-atm <- read.csv("~/Desktop/R/CreativeClusters/cat_table/atm2.csv")
-bridge <- read.csv("~/Desktop/R/CreativeClusters/cat_table/bridge2.csv")
-libraries <- read.csv("~/Desktop/R/CreativeClusters/cat_table/library2.csv")
-museums <- read.csv("~/Desktop/R/CreativeClusters/cat_table/museums2.csv")
-books <- read.csv("~/Desktop/R/CreativeClusters/cat_table/books2.csv")
-hotel <- read.csv("~/Desktop/R/CreativeClusters/cat_table/hotel2.csv")
-station <- read.csv("~/Desktop/R/CreativeClusters/cat_table/station2.csv")
-theatre <- read.csv("~/Desktop/R/CreativeClusters/cat_table/theatre2.csv")
+atm <- read.csv("./atm2.csv")
+bridge <- read.csv("./bridge2.csv")
+libraries <- read.csv("./library2.csv")
+museums <- read.csv("./museums2.csv")
+books <- read.csv("./books2.csv")
+hotel <- read.csv("./hotel2.csv")
+station <- read.csv("./station2.csv")
+theatre <- read.csv("./theatre2.csv")
 
-atm_recc  <- read.csv("~/Desktop/R/CreativeClusters/Recommendations/Atm_Rec_Geo.csv")
-bridge_recc  <- read.csv("~/Desktop/R/CreativeClusters/Recommendations/Bridge_Rec_Geo.csv")
-libraries_recc  <- read.csv("~/Desktop/R/CreativeClusters/Recommendations/Library_Rec_Geo.csv")
-museums_recc  <- read.csv("~/Desktop/R/CreativeClusters/Recommendations/Museum_Rec_Geo.csv")
-books_recc  <- read.csv("~/Desktop/R/CreativeClusters/Recommendations/Books_Rec_Geo.csv")
-hotel_recc  <- read.csv("~/Desktop/R/CreativeClusters/Recommendations/Hotels_Rec_Geo.csv")
-station_recc  <- read.csv("~/Desktop/R/CreativeClusters/Recommendations/Station_Rec_Geo.csv")
-theatre_recc  <- read.csv("~/Desktop/R/CreativeClusters/Recommendations/Theatre_Rec_Geo.csv")
+atm_rec  <- read.csv("./Atm_Rec_Geo.csv")
+bridge_rec  <- read.csv("./Bridge_Rec_Geo.csv")
+libraries_rec  <- read.csv("./Library_Rec_Geo.csv")
+museums_rec  <- read.csv("./Museum_Rec_Geo.csv")
+books_rec  <- read.csv("./Books_Rec_Geo.csv")
+hotel_rec  <- read.csv("./Hotels_Rec_Geo.csv")
+station_rec  <- read.csv("./Station_Rec_Geo.csv")
+theatre_rec  <- read.csv("./Theatre_Rec_Geo.csv")
 
 atm <- atm %>% filter(!is.na(lon))
 bridge  <- bridge  %>% filter(!is.na(lon))
@@ -38,14 +39,14 @@ hotel  <- hotel  %>% filter(!is.na(lon))
 station  <- station  %>% filter(!is.na(lon))
 theatre<- theatre %>% filter(!is.na(lon))
 
-atm_recc <- atm_recc %>% filter(!is.na(lon))
-bridge_recc  <- bridge_recc  %>% filter(!is.na(lon))
-libraries_recc  <- libraries_recc  %>% filter(!is.na(lon))
-museums_recc  <- museums_recc  %>% filter(!is.na(lon))
-books_recc  <- books_recc  %>% filter(!is.na(lon))
-hotel_recc  <- hotel_recc  %>% filter(!is.na(lon))
-station_recc  <- station_recc  %>% filter(!is.na(lon))
-theatre_recc<- theatre_recc %>% filter(!is.na(lon))
+atm_rec <- atm_rec %>% filter(!is.na(lon))
+bridge_rec  <- bridge_rec  %>% filter(!is.na(lon))
+libraries_rec  <- libraries_rec  %>% filter(!is.na(lon))
+museums_rec  <- museums_rec  %>% filter(!is.na(lon))
+books_rec  <- books_rec  %>% filter(!is.na(lon))
+hotel_rec  <- hotel_rec  %>% filter(!is.na(lon))
+station_rec  <- station_rec  %>% filter(!is.na(lon))
+theatre_rec<- theatre_rec %>% filter(!is.na(lon))
 
 
 atm$icon_type <- rep(1, nrow(atm))
@@ -58,14 +59,14 @@ station$icon_type <- rep(1, nrow(station))
 theatre$icon_type <- rep(1, nrow(theatre))
 
 
-atm_recc$icon_type <- rep(2, nrow(atm_recc))
-bridge_recc$icon_type <- rep(2, nrow(bridge_recc))
-libraries_recc$icon_type <- rep(2, nrow(libraries_recc))
-museums_recc$icon_type <- rep(2, nrow(museums_recc))
-books_recc$icon_type <- rep(2, nrow(books_recc))
-hotel_recc$icon_type <- rep(2, nrow(hotel_recc))
-station_recc$icon_type <- rep(2, nrow(station_recc))
-theatre_recc$icon_type <- rep(2, nrow(theatre_recc))
+atm_rec$icon_type <- rep(2, nrow(atm_rec))
+bridge_rec$icon_type <- rep(2, nrow(bridge_rec))
+libraries_rec$icon_type <- rep(2, nrow(libraries_rec))
+museums_rec$icon_type <- rep(2, nrow(museums_rec))
+books_rec$icon_type <- rep(2, nrow(books_rec))
+hotel_rec$icon_type <- rep(2, nrow(hotel_rec))
+station_rec$icon_type <- rep(2, nrow(station_rec))
+theatre_rec$icon_type <- rep(2, nrow(theatre_rec))
 
 get_category <- function(category) {
   switch(category,
@@ -81,14 +82,14 @@ get_category <- function(category) {
 
 get_dataset <- function(dataset) {
   switch(dataset,
-         "atm_recc" = atm_recc,
-         "bridge_recc" = bridge_recc,
-         "libraries_recc" = libraries_recc,
-         "museums_recc" = museums_recc,
-         "books_recc" =books_recc,
-         "hotel_recc" =hotel_recc,
-         "station_recc"=station_recc,
-         "theatre_recc" = theatre_recc
+         "atm_recc" = atm_rec,
+         "bridge_recc" = bridge_rec,
+         "libraries_recc" = libraries_rec,
+         "museums_recc" = museums_rec,
+         "books_recc" =books_rec,
+         "hotel_recc" =hotel_rec,
+         "station_recc"=station_rec,
+         "theatre_recc" = theatre_rec
   )
 }
 
@@ -96,14 +97,14 @@ get_dataset <- function(dataset) {
 
 function(input, output, session) {
   
-  #museums <- read.csv("~/Desktop/R/CreativeClusters/creative-master/museums.csv")
+  #museums <- read.csv("/3kurs/creative_clusters_final/creative-master/museums.csv")
   #museums <- museums[-c(17, 3, 9, 47), ]
   #df <- museums
-  #coordinates(df) <- ~lon+lat
+  #coordinates(df) <- lon+lat
   
-  #atm <- read.csv("~/Desktop/R/CreativeClusters/Atm_Rec_Geo.csv")
+  #atm <- read.csv("/3kurs/creative_clusters_final/Atm_Rec_Geo.csv")
   #df <- atm
-  #coordinates(df) <- ~lon+lat
+  #coordinates(df) <- lon+lat
   
   datasetInput <- reactive({
     
@@ -124,8 +125,8 @@ function(input, output, session) {
     
     leafIcons <- icons(
       iconUrl = ifelse(df$icon_type == 1,
-                       "http://r.piterdata.ninja/files/3kurs/create/1497642650_map-marker.png",
-                       "http://r.piterdata.ninja/files/3kurs/create/1497644002_FlagRed.png"
+                       "http://r.piterdata.ninja/files/3kurs/creative_clusters_final/1497642650_map-marker.png",
+                       "http://r.piterdata.ninja/files/3kurs/creative_clusters_final/1497644002_FlagRed.png"
       ),
       iconWidth = 30, iconHeight = 30,
       iconAnchorX = 22, iconAnchorY = 94
